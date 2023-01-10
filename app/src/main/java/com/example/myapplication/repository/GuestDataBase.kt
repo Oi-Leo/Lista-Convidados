4401104a8523b99bd2b018fcf56c1f0b3d71243c
Leonardo <leoasrgl@gmail.com>
2023-01-09 21:25:54 -0300

    fixed name column

--- app/src/main/java/com/example/myapplication/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/myapplication/repository/GuestDataBase.kt
@@ -16,7 +16,7 @@ class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VE
         db.execSQL(
             "CREATE TABLE " + DataBaseConstants.GUEST.TABLE_NAME + " (" +
                     DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement," +
-                     DataBaseConstants.GUEST.COLUMNS.NAME + "text," +
+                     DataBaseConstants.GUEST.COLUMNS.NAME + " text," +
                      DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
     }
 
